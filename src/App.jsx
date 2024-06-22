ffc12d526b1ec1dea4d58e5ad04488e543fb8fac
JatinShimpi <shimpijatin184@gmail.com>
2024-06-22 16:18:59 +0530

    started a new project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Pricing from "./Pages/Pricing";
+import Homepage from "./Pages/Homepage";
+import PageNotFound from "./Pages/PageNotFound";
+import AppLayout from "./Pages/AppLayout";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/app" element={<AppLayout />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
