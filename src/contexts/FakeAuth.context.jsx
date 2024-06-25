66c6d93e5d33f0bca1b26c394fe5d8b89782b876
JatinShimpi <shimpijatin184@gmail.com>
2024-06-25 16:41:46 +0530

    test

--- src/contexts/FakeAuth.context.jsx
+++ src/contexts/FakeAuth.context.jsx
@@ -20,8 +20,8 @@ function reducer(state,action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "jatin",
+  email: "jatin@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
